b705114c0f850a466180b91b76acb5e354f6c31b
Alex <alesandi.uk@gmail.com>
2020-09-08 17:43:28 +0100

    5-commit
    User interface

---
+++ CatalogoPeliculas/src/second/peliculas/servicio/ICatalogo.java
@@ -0,0 +1,17 @@
+package second.peliculas.servicio;
+
+public interface ICatalogo {
+    
+    String NOMBRE_RECURSO = "peliculas.txt";
+    
+    
+    
+    void agregarPelicula(String nombrePelicula);
+    
+    void listarPeliculas();
+    
+    void buscarPeliculas(String buscar);
+    
+    void iniciarCatalogoPeliculas();
+    
+}
