dc0734c684478dc7102b65fef3bad8b97e4163e1
Alex <alesandi.uk@gmail.com>
2020-09-08 17:12:09 +0100

    commit-3
    Creating class "DATOS" and all the methods.

---
+++ CatalogoPeliculas/src/second/peliculas/datos/AccesoDatos.java
@@ -0,0 +1,22 @@
+package second.peliculas.datos;
+
+import java.util.List;
+import second.peliculas.domain.Pelicula;
+import second.peliculas.excepciones.AccesoDatosEx;
+import second.peliculas.excepciones.*;
+
+public interface AccesoDatos {
+
+    boolean existe(String nombreRecurso) throws AccesoDatosEx;
+
+    List<Pelicula> listar(String nombreRecurso) throws LecturaDatosEx;
+    
+    void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx;
+    
+    String buscar (String nombreRecurso, String buscar) throws LecturaDatosEx;
+    
+    void crear (String nombreRecurso) throws AccesoDatosEx;
+    
+    void borrar (String nombreRecurso) throws AccesoDatosEx;
+
+}
